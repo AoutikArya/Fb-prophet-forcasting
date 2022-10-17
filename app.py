e5cb1798f80b241c76e79fef858db6dff30480c6
Aoutik Arya <90469048+AoutikArya@users.noreply.github.com>
2022-10-17 12:53:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+"""System module."""
+import base64
 import pandas as pd
 from prophet import Prophet
 import streamlit as st
@@ -12,10 +14,11 @@ if df is not None:
     ctm=list(df.columns.values)
     ctm=[e.lower() for e in ctm ]
     if 'date' not in ctm :
-        st.write('Invalid dataset no feature like date')            
-    else:        
+        st.write('Invalid dataset no feature like date')
+    else:
         with col1:
-            y=st.selectbox('Select a feature to be forcasted',df.columns.values[df.columns.str.lower()!='date'])
+            y=st.selectbox('Select a feature to be forcasted',
+                           df.columns.values[df.columns.str.lower()!='date'])
         with col2:
             seasonality=st.selectbox('Choose seasonality',['Daily','Monthly','Yearly'])
         with col3:
@@ -25,7 +28,7 @@ if df is not None:
                 period=st.selectbox('Period',range(1,121))
             else:
                 period=st.selectbox('Period',range(1,11))
-    df.columns= df.columns.str.lower()   
+    df.columns= df.columns.str.lower()
     df.rename(columns={'date':'ds',y.lower():'y'},inplace=True)
     df=df[['ds','y']]
     df['ds']=pd.to_datetime(df['ds'])
@@ -41,10 +44,10 @@ if df is not None:
     predict=model.predict(future)
     idx2=len(predict)-1
     if st.button('Predict'):
-        st.write(f"Value changed from   **{round(df['y'][idx],2)}**   as on {df['ds'][idx]} to    ***{round(predict['yhat'][idx2],2)}***   as on {predict['ds'][idx2]}")
+        st.write(f"Value changed from   **{round(df['y'][idx],2)}**   as on {df['ds'][idx]} to 
+                 ***{round(predict['yhat'][idx2],2)}***   as on {predict['ds'][idx2]}")
         st.pyplot(model.plot(predict,xlabel='Date',ylabel=y,include_legend=True))
         st.pyplot(model.plot_components(predict))
-import base64
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
